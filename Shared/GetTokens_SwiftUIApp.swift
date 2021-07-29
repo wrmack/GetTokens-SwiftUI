1873e2ac98073b8e8d132df88952e61e201c46db
Warwick McNaughton <warwick.mcnaughton@gmail.com>
2021-07-30 06:27:58 +1200

    Initial Commit

---
+++ Shared/GetTokens_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetTokens_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Warwick McNaughton on 30/07/21.
+//
+
+import SwiftUI
+
+@main
+struct GetTokens_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
